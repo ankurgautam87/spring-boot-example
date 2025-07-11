3b186951d7d7412756bb50362561b822305b94fe
root <root@localhost>
2025-07-11 14:13:44 +0000

    WIP

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
@@ -6,22 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 /**
  * Created by nydiarra on 06/05/17.
  */
 @Configuration
-@EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfig implements AuthorizationServerConfigurer {
 
 	@Value("${security.jwt.client-id}")
 	private String clientId;
@@ -53,16 +57,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("permitAll()");
+    }
+
+
 	@Override
-	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
-		configurer
-		        .inMemory()
-		        .withClient(clientId)
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+        builder.withClient(clientId)
 				.secret(passwordEncoder.encode(clientSecret))
 		        .authorizedGrantTypes(grantType)
 		        .scopes(scopeRead, scopeWrite)
 		        .resourceIds(resourceIds);
-	}
+
+    }
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -73,5 +83,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		        .tokenEnhancer(enhancerChain)
 		        .authenticationManager(authenticationManager);
 	}
+
+
 
 }
